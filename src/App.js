457840f9c18b9f3333b2ee58022d2a4de1c0f338
umbongo <49604919+umbongo@users.noreply.github.com>
2020-02-20 15:24:25 +1100

    adding project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          --+ This is a test +--
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          learn react
+        </a>
+      mapped volumes, used: <br></br>
+          docker run -p 3000:3000 -v /app/node_volumes -v $(pwd):/app frontend
+          </header>
+    </div>
+  );
+}
+
+export default App;
