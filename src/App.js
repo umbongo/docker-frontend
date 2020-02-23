1337822f474e88dad9d340e04c7b5575670c781f
umbongo <49604919+umbongo@users.noreply.github.com>
2020-02-23 15:12:29 +1100

    changed readme

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          --+ This is a test +--
+          --+ This is another test +--
         </p>
         <a
           className="App-link"
